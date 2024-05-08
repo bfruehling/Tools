a40dd0b126854871ce3bc0ec88e243a61febe55d
Brian Fruehling - Network <fruehling_brian@network.lilly.com>
2024-05-08 12:14:14 -0400

    updating help cleaning up whitespace

--- New-Password.ps1
+++ New-Password.ps1
@@ -3,11 +3,11 @@
 Creates a new random password, to replace Windows Powershell system.web.security GeneratePassword Method
 
 .DESCRIPTION
-The function takes 0-6 parameters to create a new random password. The length of the password, the minimum number of 
+The function takes 0-9 parameters to create a new random password. The length of the password, the minimum number of 
 each character class, and whether the output should be a secure string can all be specified.  The minimum password
-length is 4, the maximum is the Int32 limit and the password will always have at least 1 character from each character class; 
+length is 4, the maximum is the Int32 limit and the password will have characters from each character class; 
 uppercase, lowercase, numeric and special characters. If the sum of all of the minimum values is greater than the specified 
-length, the length will be increase to the sum of the minimum values.
+length, the length will be increased to the sum of the minimum values.  The character classes can be specified if necessary.
 
 .NOTES
 Date Written: Oct-2021
@@ -22,42 +22,42 @@ System.Security.SecureString
 System.String if -insecure flag is used
 
 .EXAMPLE
-New-Password -length 20 -minspecial 5 -insecure
+New-Password -length 20 -minspecial 5 
 
 Set a length of 20 with at least 5 special characters and output as a plain text string
 .EXAMPLE
-New-Password -length 15 -minspecial 3
+New-Password -length 15 -minspecial 3 -secure
 
 Set a length of 15 with at least 3 special characters and output as a secure string object
 .EXAMPLE
-New-Password
+New-Password -length 40 -specials '-_.~'
 
-Use the defaults of a length of 12 and 1 special character and output a secure string object
+Generate a password with a length of 40 and using only the specified special characters
 #>
 Function New-Password {
     [CmdletBinding()]
     param(
         #Specifies the desired length of the password, 12 is the default.  Minimum length of 4, length will be overridden if the sum of all the Min values is greater
         [ValidateRange(4, [Int32]::MaxValue)]
-        [int] $Length = 12,
+        [int]$Length = 12,
         #string of allowed uppercase characters
-        [string] $Uppers='ABCDEFGHIJKLMNOPQRSTUVWXYZ',
+        [string]$Uppers='ABCDEFGHIJKLMNOPQRSTUVWXYZ',
         #string of allowed lowercase characters
-        [string] $Lowers='abcdefghijklmnopqrstuvwxyz',
-        #string of allowed digits
-        [string]  $Digits='0123456789',
+        [string]$Lowers='abcdefghijklmnopqrstuvwxyz',
+        #string of allowed numeric characters
+        [string]$Digits='0123456789',
         #string of allowed special characters
-        [string] $Specials = '!@#$%^&*()_-+=[{]};:<>|./?',
-        #Specifies the minimum number of Special characters in the password, 1 is the default. 
-        [int] $MinSpecial = 1,
-        #Specifies the minimum number of Upper Case characters in the password, 1 is the default. 
-        [int] $MinUpper = 1,
-        #Specifies the minimum number of Lower Case characters in the password, 1 is the default. 
-        [int] $MinLower = 1,
-        #Specifies the minimum number of Numeric characters in the password, 1 is the default. 
-        [int] $MinDigit = 1,
+        [string]$Specials = '!@#$%^&*()_-+=[{]};:<>|./?',
+        #Specifies the minimum number of special characters in the password, 1 is the default. 
+        [int]$MinSpecial = 1,
+        #Specifies the minimum number of upper case characters in the password, 1 is the default. 
+        [int]$MinUpper = 1,
+        #Specifies the minimum number of lower case characters in the password, 1 is the default. 
+        [int]$MinLower = 1,
+        #Specifies the minimum number of numeric characters in the password, 1 is the default. 
+        [int]$MinDigit = 1,
         #Specifies the password to be output in plain text
-        [switch] $Secure
+        [switch]$Secure
     )
        
     $CharacterList = @{
