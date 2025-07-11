269ef48bf2366624c35018e621f7395bc83456bf
Brian Fruehling - Network <fruehling_brian@network.lilly.com>
2025-07-11 13:30:19 -0400

    script to rotate access keys for aws stored credentials

---
+++ Update-AccessKeys.ps1
@@ -0,0 +1,18 @@
+Param(
+  [Parameter(mandatory=$true,ValueFromPipelineByPropertyName=$true)][string]$userName,
+  [Parameter(mandatory=$true,ValueFromPipelineByPropertyName=$true)][string]$profileName
+)
+
+process {
+  #get current key
+  $oldKey = Get-IAMAccessKey -username $userName -ProfileName $profileName -Region cn-northwest-1
+  #create new access key
+  $newKey=New-IAMAccessKey -userName $userName -ProfileName $profileName -Region cn-northwest-1
+  #update credential
+  Set-AWSCredential -StoreAs $profileName -AccessKey $newKey.AccessKeyId -SecretKey $newKey.SecretAccessKey 
+  #delete old access key
+  start-sleep -seconds 5
+  Remove-IAMAccessKey -AccessKeyId $oldKey.AccessKeyId -ProfileName $profileName -username $userName -Confirm:$false -Region cn-northwest-1
+  Get-IAMAccessKey -username $userName -ProfileName $profileName -Region cn-northwest-1
+  write-host "Updated Access Key for $userName in $profileName"
+}
