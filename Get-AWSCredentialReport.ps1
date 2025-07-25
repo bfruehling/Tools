a6b6455618edd88ec33bf84617001e0d1c4a27bd
Brian Fruehling - Network <fruehling_brian@network.lilly.com>
2025-07-25 12:53:30 -0400

    add process block

--- Get-AWSCredentialReport.ps1
+++ Get-AWSCredentialReport.ps1
@@ -1,21 +1,30 @@
 Param(
-  [Parameter(Mandatory=$true)]
+  [Parameter(ValueFromPipelineByPropertyName,Mandatory=$true)]
   [string]$username
 )
 
-$profiles = Get-AWSCredentials -ListProfileDetail 
-$output = foreach ($profile in $profiles) {
-  $compPct = $profiles.IndexOf($profile) / $profiles.Count * 100
-  write-progress -activity "Processing Profile" -status $profile.profileName -PercentComplete $compPct
-  $user=get-IAMUser -userName $username -profileName $profile.profileName
-  $mfa=Get-IAMMfaDevice -userName $username -profileName $profile.profileName
-  $AK=Get-IAMAccessKey -userName $username -profileName $profile.profileName
-  [psCustomObject]@{ 
-    ARN=$user.arn
-    profile=$profile.profileName
-    LastUsed=$user.PasswordLastUsed
-    MFA=$MFA.SerialNumber
-    AKCreateDate=$AK.createDate
-  }
-} 
-$output | ft -a
+begin {
+  $profiles = Get-AWSCredentials -ListProfileDetail 
+  $output = @()
+}
+
+process {
+  $output += foreach ($profile in $profiles) {
+    $compPct = $profiles.IndexOf($profile) / $profiles.Count * 100
+    write-progress -activity "Processing Profile" -status $profile.profileName -PercentComplete $compPct
+    $user=get-IAMUser -userName $username -profileName $profile.profileName
+    $mfa=Get-IAMMfaDevice -userName $username -profileName $profile.profileName
+    $AK=Get-IAMAccessKey -userName $username -profileName $profile.profileName
+    [psCustomObject]@{ 
+      ARN=$user.arn
+      profile=$profile.profileName
+      LastUsed=$user.PasswordLastUsed
+      MFA=$MFA.SerialNumber
+      AKCreateDate=$AK.createDate
+    }
+  } 
+}
+
+end {
+  $output
+}
